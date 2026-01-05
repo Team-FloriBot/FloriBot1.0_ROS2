ce1a9541d229e2182774d853f905a2c238f23dfa
FabioSchaaf <fabio.sch2af@gmail.com>
2026-01-05 18:27:13 +0100

    Update base_launch.py
    Angepasst für Schlupf-Kompensation

--- src/base/launch/base_launch.py
+++ src/base/launch/base_launch.py
@@ -11,7 +11,22 @@ def generate_launch_description():
     # Config paths
     kinematics_config = os.path.join(pkg_base, 'params', 'kinematics_params.yaml')
     hardware_config = os.path.join(pkg_base, 'params', 'hardware_params.yaml')
-
+# 1. RF2O Laser Odometry Node (Berechnet die "Wahrheit" aus dem LiDAR)
+    rf2o_node = Node(
+        package='rf2o_laser_odometry',
+        executable='rf2o_laser_odometry_node',
+        name='rf2o_laser_odometry',
+        output='screen',
+        parameters=[{
+            'laser_scan_topic': '/scan',          # Dein LiDAR-Topic
+            'odom_topic': '/odom_lidar',          # Das Input-Topic für deinen Kinematics-Node
+            'publish_tf': False,                  # WICHTIG: Kein zweites TF senden!
+            'base_frame_id': 'base_link',
+            'odom_frame_id': 'odom',
+            'init_pose_from_topic': '',
+            'freq': 20.0                          # Update-Rate in Hz
+        }]
+    )
     # 1. Kinematics Node
     kinematics_node = Node(
         package='base',
@@ -31,9 +46,6 @@ def generate_launch_description():
         parameters=[hardware_config]
         
     )
-
-
-    
 
     return LaunchDescription([
         kinematics_node,
